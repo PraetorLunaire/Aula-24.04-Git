ad94a0dc354bf8d6dc742d1452c84a1df6a221df
Rubens <rubens.paulico@hotmail.com>
2019-04-24 14:31:17 -0300

    Mostra a mensagem de

--- Aula 24.04 Git/ViewController.swift
+++ Aula 24.04 Git/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("Carregou a primeira tela")
+        
+        print("Isso deveria estar aqui")
     }
 
 
